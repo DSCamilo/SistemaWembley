07d7b7cd1ff385e08042b70977092ac2e14636e9
BrayanRiascos <riascos11brayan@gmail.com>
2021-06-11 15:51:31 -0500

    3:51

---
+++ SistemaWembley/src/control/Inicio.java
@@ -0,0 +1,16 @@
+
+package control;
+
+import vista.IU_AccederSistema;
+
+
+public class Inicio {
+
+  
+    public static void main(String[] args) {
+       
+        IU_AccederSistema iniciar=new IU_AccederSistema();
+        iniciar.setVisible(true);
+    }
+    
+}
