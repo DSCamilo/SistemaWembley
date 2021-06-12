836c44cce947594d025eb5b64b9bb96e2bb8bf42
Camilo Benitez <camilobenitez@unimayor.edu.co>
2021-06-11 21:50:24 -0500

    Inicio

--- SistemaWembley/src/vista/Inicio.java
+++ SistemaWembley/src/vista/Inicio.java
@@ -1,14 +1,13 @@
 
-package control;
-
-import vista.IU_AccederSistema;
+package vista;
 
 
 public class Inicio {
 
-  
+   
     public static void main(String[] args) {
-       
+        
+        
         IU_AccederSistema iniciar=new IU_AccederSistema();
         iniciar.setVisible(true);
     }
